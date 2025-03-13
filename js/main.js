7affd8ae3c6180784b8cbd47cab196f444feedc7
NewMoon <149091994+rakazabra@users.noreply.github.com>
2025-03-13 12:48:11 -0400

    finsished

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+  let age = 15;
+  const name = "Anatolii";
+  let isStudent = true;
+  const myString = "хто як не я, коли як не зараз";
+  let myNumber = 19;
+  const myNull = null;
+
+
+        console.log(age)
+        console.log(name)
+        console.log(isStudent)
+        console.log(myString)
+        console.log(myNumber + 10);
+
+        userName = prompt("Введіть своє ім’я: ")
+        alert(userName)
+
+
+        userAnswer = confirm("Ви дійсно хочете покинути сторінку? ")
+        console.log(userAnswer);
+
+        alert('Увага! Дія може бути небезпечною.')
+        confirm('Ви впевнені, що хочете продовжити?')
